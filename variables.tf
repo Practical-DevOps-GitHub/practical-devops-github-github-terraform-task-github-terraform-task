24571e2644066b51d3e41b3145d95c3069f8a485
Catherine AMF <catherineamf@gmail.com>
2024-12-11 20:09:50 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "github_token" {
+  description = "The GitHub personal access token for authentication"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or username that owns the repository"
+  type        = string
+}
